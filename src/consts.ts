555f553f643e722ed0159cabd3ffd35abe974259
sovdevs <sovdevs@pm.me>
2025-02-28 11:30:26 +0100

    new site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "A Data Engineer's Blog";
+export const SITE_DESCRIPTION = "Showcasing my work in this area.";
